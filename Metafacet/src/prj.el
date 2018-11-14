a00a5583d5ad6b1bbbf2c23bfda97cd571e20eb5
Shane Sheehan <sheehas1@tcd.ie>
2018-11-14 17:17:40 +0000

    New plugin Metafacet

---
+++ Metafacet/src/prj.el
@@ -0,0 +1,16 @@
+(jdee-project-file-version "1.0")
+(jdee-set-variables
+ '(jdee-project-name "Mosaic")
+ '(jdee-db-option-application-args nil)
+ '(jdee-db-read-app-args t)
+ '(jdee-global-classpath (quote ("." "../lib/teccli.jar" "../lib/idx.jar" "../lib/prefuse.jar" "../lib/colt.jar" "../lib/je.jar"))) ;; "../../../src/")))
+ '(jdee-run-working-directory ".")
+ '(jdee-run-option-application-args nil)
+ '(jdee-run-read-app-args t)
+ '(jdee-sourcepath (quote (".")))
+ '(jdee-compile-option-sourcepath (quote (".")))
+ '(jdee-compile-option-directory "../classes")
+ '(jdee-run-application-class "")
+ '(jdee-compile-option-debug (quote ("all" (t nil nil)))))
+;;; JDE Project file
+;;; PRJ.EL ends here
